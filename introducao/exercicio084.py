a9e9e563a69e99a0280f76d1dda8d3d2c721eb26
Larissa-D-Gomes <larissadgomes2001@gmail.com>
2020-07-19 22:27:27 -0300

    83-86

--- introducao/exercicio084.py
+++ introducao/exercicio084.py
@@ -9,13 +9,47 @@ B) Uma listagem com as pessoas mais pesadas.
 C) Uma listagem com as pessoas mais leves.
 """
 
-
-
-
-
-
 def main():
-    pass
+    try:
+        aux = []
+        lista = []
+        aux.append(input('Digite o nome: '))
+        peso = float(input('Digite o peso: '))
+        if peso < 0.0:
+            raise ValueError
+        aux.append(peso)
+        lista.append(aux[:])
+        aux.clear()
+        leitura = input('Insira qualquer tecla para continuar.\n'
+                        'Digite [P/p] para para o programa: ').upper()
+        maior = menor = lista[0][1]
+
+        while leitura != 'P':
+            aux.append(input('Digite o nome: '))
+            peso = float(input('Digite o peso: '))
+            if peso < 0.0:
+                raise ValueError
+            aux.append(peso)
+            lista.append(aux[:])
+            aux.clear()
+            if peso > maior:
+                maior = peso
+            elif peso < menor:
+                menor = peso
+            leitura = input('Insira qualquer tecla para continuar.\n'
+                            'Digite [P/p] para para o programa: ').upper()
+
+        print(f'Foram cadastradas {len(lista)} pessoas.')
+        for i in lista:
+            if i[1] == maior:
+                print(f'{i[0]} eh uma pessoa com maior peso.')
+        for i in lista:
+            if i[1] == menor:
+                print(f'{i[0]} eh uma pessoa com menor peso.')
+
+
+    except ValueError:
+        print('Dado invalido.')
 
 
 if __name__ == '__main__':
--- introducao/exercicio085.py
+++ introducao/exercicio085.py
@@ -1,8 +1,9 @@
 """
 EXERCÍCIO 085: Listas com Pares e Ímpares
 
-Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha
-separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os
+em uma lista única que mantenha separados os valores pares e ímpares.
+No final, mostre os valores pares e ímpares em ordem crescente.
 """
 
 
@@ -11,7 +12,25 @@ separados os valores pares e ímpares. No final, mostre os valores pares e ímpa
 
 
 def main():
-    pass
+    try:
+        lista = [[],[]]
+        for x in range(7):
+            valor = int(input('Digite o numero: '))
+            if valor % 2 == 0:
+                lista[0].append(valor)
+            else:
+                lista[1].append(valor)
+        lista[0].sort()
+        print(f'PARES: {lista[0]}')
+        lista[1].sort()
+        print(f'IMPARES: {lista[1]}')
+
+    except ValueError:
+        print('Dado invalido.')
+
+
+if __name__ == '__main__':
+    main()
 
 
 if __name__ == '__main__':
